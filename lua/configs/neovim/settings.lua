88fcfc6e3477ca93607bd3f7c60cc834db2999c7
LuiggiPasacheL <luiggi.pasache.lopera@gmail.com>
2024-04-19 18:44:32 -0500

    Add command to run command in save

--- lua/configs/neovim/settings.lua
+++ lua/configs/neovim/settings.lua
@@ -32,4 +32,33 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
+local autosave_buffer = function(output_bufnr, pattern, command)
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        group = vim.api.nvim_create_augroup("AutoRunOnSave", { clear = true }),
+        pattern = pattern,
+        callback = function()
+            local append_data = function(_, data)
+                if data then
+                    vim.api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
+                end
+            end
+
+            vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, {})
+            vim.fn.jobstart(command, {
+                stdout_buffered = true,
+                on_stdout = append_data,
+                on_stderr = append_data,
+            })
+        end
+    })
+end
+
+vim.api.nvim_create_user_command("AutoRunOnSave", function()
+    print("AutoRunOnSave starts now...")
+    local bufnr = vim.fn.input "Bufnr: "
+    local pattern = vim.fn.input "pattern: "
+    local command = vim.split(vim.fn.input "command: ", " ")
+    autosave_buffer(tonumber(bufnr), pattern, command)
+end, {})
+
 vim.g.copilot_assume_mapped = true -- Tab to accept completion
